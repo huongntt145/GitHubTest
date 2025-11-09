c8b3575081280cf444f65b113a55dc943ade48bf
huongntt145 <47719441+huongntt145@users.noreply.github.com>
2025-11-09 16:31:56 +0700

    Update Constants.java

--- src/com/github/desktop/auto/test/Constants.java
+++ src/com/github/desktop/auto/test/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
 	public static final String PATH = "/Users/jen/Downloads/Selenium/lib/chromedriver";
 	public static final String URL = "https://github.com/SeleniumHQ";
 
+	
 }
